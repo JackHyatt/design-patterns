f2278e6c89e1aa07add1b94eddc5ab6ba104a204
Jack H <jhyatt7331@gmail.com>
2022-02-19 23:20:03 -0500

    Added java doc

--- singleton/MathGame.java
+++ singleton/MathGame.java
@@ -1,12 +1,20 @@
 import java.util.Random;
 import java.util.Scanner;
 
+/**
+ * Class for a math game that asks equations and gets an answer from the user. Only allows one instance of the class
+ * @author Jack Hyatt
+ */
 public class MathGame{
     private static MathGame mathGame;
     private int score;
     private Random rand;
     private Scanner reader;
     private String[] operands;
+    /**
+     * Constructor that initializes the random seed, the scanner, the score, and the operands.
+     * Is private so that the only way to create an instance is through the getInstance method
+     */
     private MathGame(){
         this.rand = new Random();
         this.reader = new Scanner(System.in);
@@ -15,12 +23,19 @@ public class MathGame{
         System.out.println("Let's have fun with Math!");
     }
 
+    /**
+     * Method to create an instance of the class if there isn't one, and return the sole instance of the class
+     * @return the sole instance of the class
+     */
     public static MathGame getInstance(){
         if(mathGame==null)
             mathGame = new MathGame();
         return mathGame;
     }
 
+    /**
+     * Method to play the game. Asks each round if the user wants to play, and does so accordingly.
+     */
     public void play(){
         String playOrQuit = "P";
         while(!playOrQuit.equalsIgnoreCase("Q")){
@@ -40,14 +55,25 @@ public class MathGame{
         
     }
 
+    /**
+     * Method to play the round. Tells the user the equation and gets their response.
+     * @return true if the user got the answer right and false if they didnt.
+     */
     private boolean playRound(){
         int[] nums = new int[]{rand.nextInt(101),rand.nextInt(101)};
         String operator = operands[rand.nextInt(4)];
         double answer = getAnswer(nums, operator);
         System.out.println("Round your answer to 1 decimal");
         System.out.println(nums[0] + operator + nums[1] +"= ");
-        double userAnswer = Double.parseDouble(reader.next());
-        reader.nextLine();// To grab the extra new line from sys in.
+        double userAnswer;
+        while(true){
+            try{
+                userAnswer = Double.parseDouble(reader.nextLine());
+                break;
+            }catch(NumberFormatException e){
+                System.out.println("That is not a number, try answering again.");
+            }
+        }
         if(userAnswer == answer){
             System.out.println("You got it!");
             return true;
